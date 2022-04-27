4a24f12e5130aaca793ead594a49ca694267ee52
Rohat04 <ozkocrohat@gmail.com>
2022-04-27 12:52:42 +0200

    SpeletJS

--- js.js
+++ js.js
@@ -1,5 +1,9 @@
+let dealerScore = document.querySelector(".dealer-score");
+let playerScore = document.querySelector(".player-score");
+
 let playerTotal = 0;
-let dealerTotal = 0;
+let dealerTotal = Math.floor(Math.random() * 14 + 2);
+dealerScore.innerHTML = dealerTotal;
 
 /*
 let player1Total = 0;
@@ -11,42 +15,73 @@ let player5Total = 0;
 detta är varför man användrr variabler istället för den där uppe
 let players = [ 0, 0, 0, 0, 0 ];
 */
+
 const hitButton = document.querySelector(".hit-btn");
 const stayButton = document.querySelector(".stay-btn");
+const restartButton = document.querySelector(".restart-btn");
+const playStatusText = document.querySelector(".play-status");
 
 hitButton.addEventListener("click", hitMe);
 stayButton.addEventListener("click", stayB);
 
 function hitMe() {
   playerTotal += Math.floor(Math.random() * 14 + 2);
-  
-  
+  playerScore.innerHTML = playerTotal;
   if (playerTotal == 21) {
-    alert("YOU WON!!!!");
-    playerTotal = 0;
+    hitButton.disabled = true;
+    stayButton.disabled = true;
+    playStatusText.innerHTML = "You won!";
   } else if (playerTotal > 21) {
-    alert("you lose");
-    playerTotal = 0;
+    hitButton.disabled = true;
+    stayButton.disabled = true;
+    playStatusText.innerHTML = "You lost!";
   }
-  document.querySelector(".player-score").innerHTML = playerTotal;
 }
 
 function stayB() {
-    dealerTotal += Math.floor(Math.random() * 14 + 2);
-
-    document.querySelector(".dealer-score").innerHTML = dealerTotal;
-    document.querySelector(".player-score").innerHTML = playerTotal;
-
-    if (playerTotal > dealerTotal){
-        alert("YOU WON!!!!");
-        playerTotal = 0;
-        dealerTotal = 0;
-    } else {
-        alert("You lost!");
-        playerTotal = 0;
-        dealerTotal = 0;
-    }
-    document.querySelector(".dealer-score").innerHTML = dealerTotal;
-    document.querySelector(".player-score").innerHTML = playerTotal;
+  dealerTotal += Math.floor(Math.random() * 14 + 2);
+  dealerScore.innerHTML = dealerTotal;
+  hitButton.disabled = true;
+  if (dealerTotal >= playerTotal && dealerTotal <= 21) {
+    hitButton.disabled = true;
+    playStatusText.innerHTML = "You lost!";
+    stayButton.disabled = true;
+  } else if (dealerTotal == 21) {
+    hitButton.disabled = true;
+    stayButton.disabled = true;
+    playStatusText.innerHTML = "You lost!";
+  } else {
+    //lägg koden nedanför i else if
+    dealerTotal > 21;
+    hitButton.disabled = true;
+    stayButton.disabled = true;
+    playStatusText.innerHTML = "You won!";
   }
+  dealerScore.innerHTML = dealerTotal;
+  document.querySelector(".player-score").innerHTML = playerTotal;
+}
 
+/*
+function stayB() {
+  dealerTotal += Math.floor(Math.random() * 14 + 2);
+  dealerScore.innerHTML = dealerTotal;
+  document.querySelector(".player-score").innerHTML = playerTotal;
+
+  if (dealerTotal > playerTotal) {
+    hitButton.disabled = true;
+    stayButton.disabled = true;
+    playStatusText.innerHTML = "You lost all your hard earned money!";
+  } else if (dealerTotal > 21) {
+    hitButton.disabled = true;
+    stayButton.disabled = true;
+    playStatusText.innerHTML = "You Won!!!!";
+  }
+  dealerScore.innerHTML = dealerTotal;
+  document.querySelector(".player-score").innerHTML = playerTotal;
+}
+
+function restartB() {
+  playerTotal = 0;
+  dealerTotal = 0;
+}
+*/
